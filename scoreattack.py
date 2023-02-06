8cce5a6fbd05c5e7c2b4655329e5055e1d9f5929
Kruasanow <73633964+Kruasanow@users.noreply.github.com>
2023-02-06 22:58:30 +0300

    fix

--- scoreattack.py
+++ scoreattack.py
@@ -1,9 +1,10 @@
-import osh, attack
+import osh
+import attack as at
 
 def level_acl():
-    bad_list = attack.checkAcl(osh.cap,attack.acl)[0]
-    bad_count = attack.checkAcl(osh.cap,attack.acl)[1]
-    bad_koef = bad_count / attack.packet_count()
+    bad_list = at.checkAcl(osh.cap,at.acl)[0]
+    bad_count = at.checkAcl(osh.cap,at.acl)[1]
+    bad_koef = bad_count / at.packet_count()
     if bad_koef >= 0.1:
         pointer = False
     else:
@@ -11,7 +12,7 @@ def level_acl():
     return bad_list, pointer
 
 def level_ttl():
-    arr = attack.checkTTL(osh.cap)
+    arr = at.checkTTL(osh.cap)
     bad_unixwindows = arr[1]/arr[2]
     if bad_unixwindows >= 0.5:
         bad_pointer = False
@@ -31,10 +32,10 @@ def level_syn():
     return True
 
 def DNSTZ():
-    return attack.DNStransferZone()
+    return at.DNStransferZone()
 
 def DNSAMPL():
-    return attack.DNSAmplification()
+    return at.DNSAmplification()
 
 def level_ssl():
     return True
