3e7cad90d8326d258d2f21c6dabafdc4e15ec0a4
Kruasanow <73633964+Kruasanow@users.noreply.github.com>
2023-02-07 22:06:08 +0300

    fix syntax

--- init_db.py
+++ init_db.py
@@ -1,13 +1,6 @@
 import os
 import psycopg2
 
-# ЭТОТ ФАЙЛ НУЖЕН ДЛЯ СОЗДАНИЯ БД
-# 1. Создай БД, юзера и назначь ему пароль в psql, дай доступ юзеру к бд
-# 2. Создай файл init_db.py в нем создаются таблицы БД
-# 3. Выполнить init_db.py для добавления контента в БД
-# 4. Подключись к БД в psql
-# 5. \c flask_db 
-
 conn = psycopg2.connect(
         host="localhost",
         database="flask_db",
@@ -32,7 +25,7 @@ cur.execute('CREATE TABLE books (id serial PRIMARY KEY,'
 
 cur.execute('INSERT INTO books (title, author, pages_num, review)'
             'VALUES (%s, %s, %s, %s)',
-            ('Dmitry Rusanow',
+            ('Dmitry R',
              'big dick',
              13,
              'A great cock!')
@@ -41,7 +34,7 @@ cur.execute('INSERT INTO books (title, author, pages_num, review)'
 
 cur.execute('INSERT INTO books (title, author, pages_num, review)'
             'VALUES (%s, %s, %s, %s)',
-            ('Ilya Piskunov',
+            ('Ilya P',
              'lil dick',
              9,
              'Another great cock!')
