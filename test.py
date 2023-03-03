340de47b9905dc75a0f6de95d8e3667e0a4318fd
Kruasanow <73633964+Kruasanow@users.noreply.github.com>
2023-03-03 20:01:35 +0300

    ya sha ebanus

--- test.py
+++ test.py
@@ -18,7 +18,12 @@ print(k)
 count_error = 0
 conn = main.get_db_connection() 
 cur = conn.cursor()
+bad = []
 for i in dns_arr:
+    # try:
+    #     print((i.dns.a))
+    # except AttributeError:
+    #      continue
     try:
         if int(i.dns.flags_response) == 0:
                 cur.execute('INSERT INTO dns_flags (id_pac, qry_class, qry_name, qry_type, flags_z, flags_truncated, flags_response, flags_recdesired, flags_opcode, count_queries, count_labels, count_auth_rr, count_answers, count_add_rr)'
@@ -40,50 +45,50 @@ for i in dns_arr:
                 )
                 )
         elif int(i.dns.flags_response) == 1:
-                cur.execute('INSERT INTO dns_flags (id_pac, qry_class, qry_name, qry_type, flags_z, flags_truncated, flags_response, flags_recdesired, flags_opcode, count_queries, count_labels, count_auth_rr, count_answers, count_add_rr, flags_authenticated, flags_authoritative, flags_rcode, flags_recavail, resp_class, resp_ttl, resp_type, response_to)'
-                'VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)',
+                cur.execute('INSERT INTO dns_flags (id_pac, qry_class, qry_name, qry_type, flags_z, flags_truncated, flags_response, flags_recdesired, flags_opcode, count_queries, count_labels, count_auth_rr, count_answers, count_add_rr, flags_authenticated, flags_authoritative, flags_rcode, flags_recavail, resp_class, resp_ttl, resp_type, response_to, a_return_rec)'
+                'VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)',
                 (str(i.dns.id),
                 str(i.dns.qry_class),
                 str(i.dns.qry_name),
                 str(i.dns.qry_type),
-                int(i.dns.flags_z),
-                int(i.dns.flags_truncated),
-                int(i.dns.flags_response),
-                int(i.dns.flags_recdesired),
-                int(i.dns.flags_opcode),
+                str(i.dns.flags_z),
+                str(i.dns.flags_truncated),
+                str(i.dns.flags_response),
+                str(i.dns.flags_recdesired),
+                str(i.dns.flags_opcode),
                 str(i.dns.count_queries),
                 str(i.dns.count_labels),
                 str(i.dns.count_auth_rr),
                 str(i.dns.count_answers),
                 str(i.dns.count_add_rr),
-                int(i.dns.flags_authenticated),
-                int(i.dns.flags_authoritative),
-                int(i.dns.flags_rcode),
-                int(i.dns.flags_recavail),
+                str(i.dns.flags_authenticated),
+                str(i.dns.flags_authoritative),
+                str(i.dns.flags_rcode),
+                str(i.dns.flags_recavail),
                 str(i.dns.resp_class),
                 str(i.dns.resp_ttl),
                 str(i.dns.resp_type),
-                str(i.dns.response_to)
+                str(i.dns.response_to),
+                str(i.dns.a)
                 )
                 )
-                try:
-                    cur.execute('INSERT INTO dns_flags (a_return_rec)'
-                    'VALUES (%s)',
-                    (str(i.dns.a))
-                    )
-                    print('try works')
-                except AttributeError:
-                    cur.execute('INSERT INTO dns_flags (a_return_rec)'
-                    'VALUES (%s)',
-                    ("---"          
-                    )
-                    )
-                    print('except works')
+                # try:
+                #     cur.execute('INSERT INTO dns_flags (a_return_rec) VALUES (%s)', (str(32)))
+                #     print('try works')
+                # except TypeError or AttributeError:
+                #     cur.execute('INSERT INTO dns_flags (a_return_rec)'
+                #     'VALUES (%s)',
+                #     ("---"          
+                #     )
+                #     )
+                #     print('except works')
     except AttributeError:
-        print('something WRONG !!!')
+        print('something WRONG !!! - ' + str(i.dns.id) + " " +str(i.dns.flags_response))
+        bad.append(i)
         count_error = count_error + 1
         continue
 print(count_error)
+print(bad)
 
 conn.commit()
 cur.close()
