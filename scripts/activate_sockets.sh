e9fdc691c85a3c4db1239ac6881a788161d97ed3
Kruasanow <rusanow.mitya@bk.ru>
2023-04-02 21:19:55 +0300

    adept

--- scripts/activate_sockets.sh
+++ scripts/activate_sockets.sh
@@ -1,1 +1,5 @@
+sudo apt install tshark
+sudo apt install postgesql
+sudo usermod -aG wireshark ubuntu18
+sudo chown ubuntu18 /usr/bin/dumpcap
 sudo setcap cap_net_raw,cap_net_admin=eip /usr/bin/python3.6
