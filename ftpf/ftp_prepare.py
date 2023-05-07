9a915da388b6b869872d9a49ae6aa0f97055e498
Kruasanow <rusanow.mitya@bk.ru>
2023-05-07 14:27:49 +0300

    +ftperr

--- ftpf/ftp_prepare.py
+++ ftpf/ftp_prepare.py
@@ -46,18 +46,33 @@ if PROJECT_PATH not in sys.path:
     sys.path.append(PROJECT_PATH)
 
 from osh import get_file, get_dname_from_db
+
 def detect_ftp_anomaly(cap):
+    from collections import Counter
+    
+    window_sizes = set()
+    command_counter = []
+
     cap = to_ftp_arr(cap)
     print(len(cap))
-    detector_brute = False #брутят
-    detector_secureport = False #админ еблан не сменил порт
-    detect_duplicate_acknowledgment = False #двойное подтверждение
-    detect_retransmission = False # проблема с передачей
-    detect_encrypted_connection = False #работа без TLS
-    detect_conf_troubles = False 
-    from collections import Counter
-    command_counter = []
-    window_sizes = set()
+    general_out = {
+                'detector_brute':[False,''],
+                'detector_secureport':[False,''],
+                'detect_duplicate_acknowledgment':[False,[]],
+                'detect_retransmission':[False,[]],
+                'detect_encrypted_connection':[False,[]],
+                'detect_conf_troubles':[False,[]],
+                'detect_window_size':[False,[]]
+                }
+    # detector_brute = False #брутят
+    # detector_secureport = False #админ еблан не сменил порт
+    # detect_duplicate_acknowledgment = False #двойное подтверждение
+    # detect_retransmission = False # проблема с передачей
+    # detect_encrypted_connection = False #работа без TLS
+    # detect_conf_troubles = False 
+
+
+
     for pac in cap:
         # print(str(pac))
         try:
@@ -70,32 +85,37 @@ def detect_ftp_anomaly(cap):
         count_port = counter['PORT']
         count_pasv = counter['PASV']
         if count_port < 2 and count_pasv > 2:
-            detector_secureport = True
+            general_out['detector_secureport'][0] = True
+            general_out['detector_secureport'][1] = f'Команды FTP поиска порта: активные - {count_pass}, пассивные - {count_pasv}'
         if count_user > 3 or count_pass > 3:
-            detector_brute = True
-
+            general_out['detector_brute'][0] = True
+            general_out['detector_brute'][1] = f'Попыток авторизации: login - {count_user}, password - {count_pass}'
         if 'tcp.analysis.retransmission' in str(cap):
-            detect_retransmission = True
+            general_out['detect_retransmission'][0] = True
+            general_out['detect_retransmission'][1].append(str(pac))
 
         if 'tcp.analysis.duplicate_acknowledgment' in str(cap):
-            detect_duplicate_acknowledgment = True  
-
+            general_out['detect_duplicate_acknowledgment'][0] = True  
+            general_out['detect_duplicate_acknowledgment'][1].append(str(pac))
         try:
-            if 'STOR' in pac.ftp.command or 'RETR' in pac.ftp.command:
+            if 'STOR' in pac.ftp.command or 'RETR' in pac.ftp.command: #НАДО ПРОВЕРИТЬ - КАКАЯ-ТО ХУЙНЯ
                 if pac.ftp_data and 'USER' not in pac.ftp.command and 'PASS' not in pac.ftp.command:
                     if 'TLS' not in pac:
-                        detect_encrypted_connection = True 
+                        general_out['detect_encrypted_connection'][0] = True
+                        general_out['detect_encrypted_connection'][1].append(str(pac)) 
         except Exception:
             pass
 
         if '530 Login incorrect' in str(pac):
-            detect_conf_troubles = True
+            general_out['detect_conf_troubles'][0] = True
+            general_out['detect_conf_troubles'][1].append(str(pac))
         
         if 'FTP-DATA' in str(pac):
             window_size = int(pac['tcp.window_size'])
             window_sizes.add(window_size)
         if len(window_sizes) > 1:
-            print('FTP traffic contains different window sizes: {}'.format(window_sizes)) #!!!!!!
-
-    return [detector_brute, detector_secureport,detect_duplicate_acknowledgment,detect_retransmission,detect_encrypted_connection,detect_conf_troubles]
-print(detect_ftp_anomaly(get_file('ftp_brute.pcapng')))
+            general_out['detect_window_size'][0] = True 
+            general_out['detect_window_size'][1].append(window_sizes)    
+    return general_out.values()
+    # return [detector_brute, detector_secureport,detect_duplicate_acknowledgment,detect_retransmission,detect_encrypted_connection,detect_conf_troubles]
+print(detect_ftp_anomaly(get_file('ftp_brute.pcapng'))) 
