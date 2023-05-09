840f35c05c07b3c2efc0268010843663a97bda7e
Kruasanow <rusanow.mitya@bk.ru>
2023-05-09 12:32:12 +0300

    color bg gr

--- graths/graths.py
+++ graths/graths.py
@@ -9,11 +9,23 @@ def do_grath(arr1, arr2, xlabel, ylabel, title):
     x = np.array(arr1)
     y = np.array(arr2)
 
-    # plt.plot(x, y)
-    plt.plot(x, y)
-    plt.xlabel(str(xlabel))
-    plt.ylabel(str(ylabel))
-    plt.title(str(title))
+    # plot the data
+    fig, ax = plt.subplots()
+    ax.plot(x, y)
+
+    # set the labels and title
+    ax.set_xlabel(str(xlabel), color='#151515')
+    ax.set_ylabel(str(ylabel), color='#151515')
+    ax.set_title(str(title), color='black')
+
+    # set the color of the spines (i.e. the frame around the plot)
+    ax.spines['bottom'].set_color('#151515')
+    ax.spines['top'].set_color('#151515')
+    ax.spines['left'].set_color('#151515')
+    ax.spines['right'].set_color('#151515')
+
+    # set the background color
+    ax.set_facecolor('#151515')
 
     # get the image as a base64-encoded string
     buffer = io.BytesIO()
@@ -24,6 +36,8 @@ def do_grath(arr1, arr2, xlabel, ylabel, title):
     graph = image_base64
     plt.close()
     return graph
+
+
 # arr1 = [1,2,1,13,2,1]
 # arr2 = [2,3,3,1,12,3]
 # print(do_grath(arr1,arr2,'x','y','title'))
@@ -37,13 +51,16 @@ def histogram(arr):
     return Response(image, mimetype='image/png')
 
 def build_circle(labels,sizes):
-
-    # import io
     # Создаем круговую диаграмму
+    colors = ['#ff9999','#66b3ff','#99ff99','#ffcc99']
     fig, ax = plt.subplots()
-    ax.pie(sizes, labels=labels, autopct='%1.1f%%')
+    fig.set_facecolor('#151515')
+    ax.pie(sizes, labels=labels, autopct='%1.1f%%', colors=colors)
     ax.axis('equal')
+    ax.set_facecolor('#151515')  # изменяем цвет заднего фона
     plt.tight_layout()
+    for text in ax.texts:
+        text.set_color('white')  # изменяем цвет всех текстов на белый
 
     # Сохраняем диаграмму в буфер
     buffer = io.BytesIO()
@@ -55,3 +72,4 @@ def build_circle(labels,sizes):
     graphic = base64.b64encode(image_png).decode('utf-8')
     plt.close()
     return graphic
+
