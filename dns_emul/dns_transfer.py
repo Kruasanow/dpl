99964ad02d76527f636a0b332cf400f80c07c574
Kruasanow <rusanow.mitya@bk.ru>
2023-05-23 23:49:13 +0300

    123

--- dns_emul/dns_transfer.py
+++ dns_emul/dns_transfer.py
@@ -1,18 +1,25 @@
 import dns.query
 import dns.zone
+import subprocess
 
 def test_dns_trans_zone(dnsserver, domain):
+    good_influence = ''
     # ask zone for domain
-    try:
-        zone = dns.zone.from_xfr(dns.query.xfr(dnsserver, domain))
-        # get all records in zone
-        for name in zone.nodes.keys():
-            good_influence = zone[name].to_text(name)
-            # print(type(zone[name].to_text(name)))
-            status = True
-    except Exception as e:
-        good_influence = 'zone transfer is not available'
-        status = False
-    return [good_influence, status]
-
-# print(test_dns_trans_zone('ns1.vkontakte.com','vk.com'))
+    # try:
+    #     zone = dns.zone.from_xfr(dns.query.xfr(dnsserver, domain))
+    #     # get all records in zone
+    #     for name in zone.nodes.keys():
+    #         good_influence = zone[name].to_text(name)
+    #         # print(type(zone[name].to_text(name)))
+    #         status = True
+    # except Exception as e:
+    #     good_influence = 'zone transfer is not available'
+    #     status = False
+    # return [good_influence, status]
+    status = False
+    subprocess.call(['./scripts/ztrans.sh',domain,dnsserver])
+    with open('dns_emul/transfer_zone.txt','r') as r:
+        good_influence = r.read()
+        status = True
+    return [good_influence,status]
+# test_dns_trans_zone('nsztm1.digi.ninja.','zonetransfer.me')
---
+++ scripts/ztrans.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+host -l $1 $2 > dns_emul/transfer_zone.txt
